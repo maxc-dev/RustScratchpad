6432cd5f64c4f0ecefa35379146a85e36b89967e
maxc-dev <mer.cartermc@gmail.com>
2026-03-31 23:01:52 +0100

    2160

---
+++ src/leetcode/2160_min_sum_four_digit_num_after_split.rs
@@ -0,0 +1,12 @@
+pub fn minimum_sum(num: i32) -> i32 {
+    let mut digits = [
+        num / 1000,
+        (num / 100) % 10,
+        (num / 10) % 10,
+        num % 10,
+    ];
+    digits.sort_unstable();
+    let new1 = digits[0] * 10 + digits[2];
+    let new2 = digits[1] * 10 + digits[3];
+    new1 + new2
+}
